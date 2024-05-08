d8ca3ca058e117904c21548167e6731384e41a67
Leedje <leedje2006@hotmail.com>
2024-05-08 11:26:18 +0200

    Update Program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,7 +2,19 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var host = new HostBuilder()
+var host = new HostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            await builder.Build().RunAsync();
+
+            // Configure HttpClient to send requests to your Arduino
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                //Voer IP Adres in
+                BaseAddress = new Uri("http://145.93.232.26/") // Replace with your actual Arduino IP
+            });
+
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
